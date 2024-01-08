b2c4c78023312c3e10378e09b5dc389bda9b1271
YouSeongJung <dpound@naver.com>
2024-01-08 19:51:18 +0900

    간단한 에러 해결

--- src/main/java/com/lubid/lubiduser/Service/DeliveryService.java
+++ src/main/java/com/lubid/lubiduser/Service/DeliveryService.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,12 +25,25 @@ public class DeliveryService {
 
     private final DeliveryAddressRepository addressRepository;
 
+    /**
+     * 유저는 자기 자신의 주소 정보만 가져와야하기 때문에 자기만 확인하는 검증 코드가 있어야한다.
+     * */
     @Transactional
     public ResponseEntity<String> insertDeliveryAddress(DeliveryAddress deliveryAddress){
+        // db data
         User finduser =  userRepository.findByUserName(deliveryAddress.getUser().getUserName());
+
+        // security context
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails = (UserDetails)principal;
+        String username = userDetails.getUsername();
 
         if(finduser == null){
             return new ResponseEntity<>("sorry not found user", HttpStatus.BAD_REQUEST);
+        }
+
+        if(!username.equals(deliveryAddress.getUser().getUserName())){
+            return new ResponseEntity<>("sorry you don't have auth", HttpStatus.FORBIDDEN);
         }
 
         deliveryAddress.setUser(finduser);
