a5a971047ced0efdba2f2994924e5c53b5731dac
dpound@naver.com <dpound@naver.com>
2024-01-12 23:56:01 +0900

    오타수정

--- src/main/java/com/lubid/lubiduser/Controller/DeliveryController.java
+++ src/main/java/com/lubid/lubiduser/Controller/DeliveryController.java
@@ -2,17 +2,12 @@ package com.lubid.lubiduser.Controller;
 
 import com.lubid.lubiduser.Service.DeliveryService;
 import com.lubid.lubiduser.Service.UserService;
-import com.lubid.lubiduser.dto.UserDto;
 import com.lubid.lubiduser.model.DeliveryAddress;
-import com.lubid.lubiduser.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 @Log4j2
@@ -32,7 +27,7 @@ public class DeliveryController {
 
     @GetMapping(value = "find/{userName}")
     public ResponseEntity findByAllDeliveryAddressoOfUser(@PathVariable String userName){
-        return new ResponseEntity<>(deliveryService.findAllDeliveryAddressofUser(userName), HttpStatus.OK);
+        return new ResponseEntity<>(deliveryService.findAllDeliveryAddressOfUser(userName), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/lubid/lubiduser/Service/DeliveryService.java
+++ src/main/java/com/lubid/lubiduser/Service/DeliveryService.java
@@ -9,13 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -50,7 +45,7 @@ public class DeliveryService {
     }
 
     @Transactional(readOnly = true)
-    public ResponseEntity findAllDeliveryAddressofUser(String username){
+    public ResponseEntity findAllDeliveryAddressOfUser(String username){
         // db data
         User finduser =  userRepository.findByUserName(username);
 
